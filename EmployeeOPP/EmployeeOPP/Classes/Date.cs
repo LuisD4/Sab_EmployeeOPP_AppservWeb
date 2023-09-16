12005687234b416667c4367caba431fb414958ee
LUIS DAVID PEDRAZA MOSQUERA <luispedraza5934@gmail.com>
2023-09-15 19:58:04 -0500

    Provando

---
+++ EmployeeOPP/EmployeeOPP/Classes/Date.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeOPP.Classes
+{
+    internal class Date
+    {
+        #region Atributos
+        int _year;
+        int _month;
+        int _day;
+        #endregion Atributos
+        public Date( int day, int month, int year)
+        {
+            _day = day;
+            _month = month; 
+            _year = year;   
+
+        }
+        public override string ToString() 
+        { 
+            //dia/mes/año
+            var dateConcatenated = _day +"/"+_month +"/"+_year;
+            return dateConcatenated;
+
+        }
+    }
+    
+}
--- EmployeeOPP/EmployeeOPP/Program.cs
+++ EmployeeOPP/EmployeeOPP/Program.cs
@@ -1,2 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using EmployeeOPP.Classes;
+
+Console.WriteLine("OOP APPLICATION");
+Console.WriteLine("_______________");
+
+//Inicializacion de la clase Date 
+Date dateObject = new Date(4,5,5655214);
+Console.WriteLine(dateObject.ToString());
+
+
